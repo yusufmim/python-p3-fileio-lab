edae9602d81a83bca20f3daad855daf78ddfb82f
yusufmim <yusufmim2001@gmail.com>
2025-05-29 13:39:22 +0300

    initial commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,25 @@
+#def write_file(file_name, file_content):
+ #   pass
+
+#def append_file(file_name, append_content):
+   # pass
+
+#def read_file(file_name):
+    #pass
+
+
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content + "\n")
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content + "\n")
+
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read().strip()  # strip() removes trailing newline for cleaner output
