d666e238f8ee2dd766af031016d595d3928f25fb
yusufmim <yusufmim2001@gmail.com>
2025-05-29 13:54:10 +0300

    commit v3

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,14 +1,13 @@
 def write_file(file_name, file_content):
     with open(f"{file_name}.txt", "w") as file:
-        file.write(file_content + "\n")
+        file.write(file_content)
 
 
 def append_file(file_name, append_content):
     with open(f"{file_name}.txt", "a") as file:
-        file.write(append_content + "\n")
+        file.write(append_content)
 
 
 def read_file(file_name):
     with open(f"{file_name}.txt", "r") as file:
-        return file.read().strip()
-
+        return file.read()
